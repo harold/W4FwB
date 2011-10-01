e51b9ca218fa894717d172e7cb26d3a8f624d019
Harold <hhausman@gmail.com>
2011-10-01 10:40:32 -0600

    First go at types.

--- migrations/001_init.rb
+++ migrations/001_init.rb
@@ -6,9 +6,16 @@ Sequel.migration do
 			String :password
 			String :nickname
 		end
+
+		create_table :types do
+			primary_key :id
+			String :name
+		end
+		%w{ Fire Ice Grass Electric Water }.each { |t| self[:types] << { name: t } }
 	end
 
 	down do
+		drop_table :types
 		drop_table :accounts
 	end
 end
